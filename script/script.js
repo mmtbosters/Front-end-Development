211553676700a848b815d9fe2ad6e47f11d18bbc
mmtbosters <45010025+mmtbosters@users.noreply.github.com>
2019-11-05 16:00:12 +0100

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,9 +1,28 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+// Dit is de functie die de menubalk laat verschijnen.
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('body nav:nth-of-type(2)');
+
+	burger.addEventListener('click', () => {
+		nav.classList.toggle('nav-active');
+	});
+
 }
 
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+// Hier word de functie gestart.
+navSlide();
+
+// Dit is de functie die de rogue laat verschijnen en het welbekende stealth geluidje laat horen.
+const rogueUnstealth = () => {
+	const gankpaladin = document.querySelector('.gankpally');
+	const rogue = document.querySelector('main article:nth-of-type(4) div img:nth-of-type(2)');
+
+
+	gankpaladin.addEventListener('click', () => {
+		rogue.classList.toggle('rogue-active');
+		document.getElementById('stealthy').play();
+	});
 }
+
+// Hier word de functie gestart.
+rogueUnstealth();
